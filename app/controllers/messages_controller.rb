00b81ad36f2552bf8d545924c6fe50148cba881a
marvinick <marviluw@gmail.com>
2014-12-07 11:56:23 -0600

    messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,51 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :show, :destroy]
+
   def index
     @messages = Message.all
   end
 
   def new
     @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "Message was created"
+      redirect_to messages_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @message.update(message_params)
+      redirect_to messages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @message.destroy
+      redirect_to messages_path
+    else
+      render message_path
+    end
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :content)
   end
 end
--- spec/features/user_creates_message_spec.rb
+++ spec/features/user_creates_message_spec.rb
@@ -7,5 +7,9 @@ feature "User creates message" do
     visit messages_path
     click_link "New Message"
     visit new_message_path
+    fill_in "Title", with: message.title
+    fill_in "Content", with: message.content
+    click_button "Submit"
+    expect(page).to have_content ''
   end
 end
