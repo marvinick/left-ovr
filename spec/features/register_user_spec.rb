104f51e8ca62bf464cbcbd1af862682107e5b265
marvinick <marviluw@gmail.com>
2014-12-11 15:04:02 -0600

    users rspec

--- spec/features/register_user_spec.rb
+++ spec/features/register_user_spec.rb
@@ -5,5 +5,22 @@ feature 'Register User' do
 
   scenario "With valid input" do
     visit users_path
+    click_link "Add User"
+    visit new_user_path
+    fill_in "Name", with: user.name
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password_digest
+    click_button "Okay"
+    expect(page).to have_content ''
+  end
+
+  scenario "With invalid input" do
+    visit users_path
+    click_link "Add User"
+    visit new_user_path
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password_digest
+    click_button "Okay"
+    expect(page).to have_content ''
   end
 end
