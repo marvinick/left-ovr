cc9654c3dc3c7910233f602625a92c82d47816ce
marvinick <marviluw@gmail.com>
2014-12-11 22:50:33 -0600

    user rspec next

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :destroy]
 
 def index
   @users = User.all
@@ -18,7 +19,23 @@ def create
   end
 end
 
+def edit; end
+
+def update
+  if @user.update(user_params)
+    redirect_to users_path
+  else
+    render :edit
+  end
+end
+
+def show; end
+
 private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
